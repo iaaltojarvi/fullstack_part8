2904320ff94f23cccdb740dfe026dd308e064a97
Inari Aaltojärvi <ext.inari.aaltojarvi@cargotec.com>
2020-08-17 10:11:45 +0300

    Updated readme. Authors view in frontend.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # fullstack_part8
+
+GraphQL library app exercise. Backend with Apollo server and frontend with Apollo client.
--- library-frontend/src/components/Authors.js
+++ library-frontend/src/components/Authors.js
@@ -1,15 +1,31 @@
-  
 import React from 'react'
+import { gql, useQuery } from '@apollo/client'
+
+const ALL_AUTHORS = gql`
+query {
+  allAuthors {
+    name
+    born
+    bookCount
+  }
+}
+`
 
 const Authors = (props) => {
+  const result = useQuery(ALL_AUTHORS)
+  console.log(result)
+
   if (!props.show) {
     return null
   }
-  const authors = []
+
+  if (result.loading) {
+    return <div>Loading authors...</div>
+  }
 
   return (
     <div>
-      <h2>Testing</h2>
+      <h2>Authors</h2>
       <table>
         <tbody>
           <tr>
@@ -21,7 +37,7 @@ const Authors = (props) => {
               books
             </th>
           </tr>
-          {authors.map(a =>
+          {result.data.allAuthors.map(a =>
             <tr key={a.name}>
               <td>{a.name}</td>
               <td>{a.born}</td>
